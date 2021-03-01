189c559092f4468c83d9e7b0a68e61a9c1dcf89b
acatzk <joshuaimalay@gmail.com>
2021-03-01 09:40:24 +0800

    feat: my file structure (#2)

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Messenger</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <h1>Hellow world</h1>
+      
+    </>
+  )
+}
