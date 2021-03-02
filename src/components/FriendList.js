0f4956eb2e9c2261546e63c7e7a0ae3a336975af
Joshua Galit <joshuaimalay@gmail.com>
2021-03-02 15:09:04 +0800

    fix: avatar alignment

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -3,9 +3,5 @@ import Friend from './Friend'
 export default function FriendList ({ friends }) {
   if (!friends) return null
 
-  return (
-    <div className="py-1">
-      { friends ? friends.map(Friend) : undefined}
-    </div>
-  )
+  return friends ? friends.map(Friend) : undefined
 }
