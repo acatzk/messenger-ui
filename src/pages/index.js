afbbb955421488be34f618e18cb8511662d1d051
Joshua Galit <joshuaimalay@gmail.com>
2021-03-19 12:52:15 +0800

    feat: user chat header

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,14 @@
-import Head from 'next/head'
-import Layout from '~/components/Layout'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import Spinner from '~/components/Spinner'
 
 export default function IndexPage() {
-  return (
-    <>
-      <Head>
-        <title>Messenger</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Layout>
-        Minim minim occaecat do duis occaecat et nostrud exercitation adipisicing culpa. Anim aute nisi cupidatat aliqua exercitation velit amet officia excepteur cillum mollit magna in. Culpa veniam mollit esse dolore. Magna ad in deserunt veniam ut non consequat dolor dolor. Voluptate cupidatat ullamco laborum non sunt deserunt. Aliquip exercitation ut eiusmod amet adipisicing proident sunt aute. Dolore eiusmod nostrud est aute et duis.
-      </Layout>
-    </>
-  )
+
+  const router = useRouter()
+
+  useEffect(() => {
+    router.push('/t/100002023141302')
+  }, [])
+
+  return <Spinner />
 }
